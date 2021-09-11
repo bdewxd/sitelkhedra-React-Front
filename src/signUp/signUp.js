5e039125b53805c33040db8903fbdf0f41f2ac60
bdewxd <nimakdk@gmail.com>
2021-09-11 19:19:20 +0100

    e

--- src/signUp/signUp.js
+++ src/signUp/signUp.js
@@ -1,23 +1,50 @@
 import css from "./signUp.css";
+import {useState} from 'react';
+
+
+
 let SignUp = ()=>{
+    let [fullname,setFullname] = useState("");
+    let [email,setEmail] = useState("");
+    let [phoneNumber,setPhoneNumber] = useState("");
+    let [password,setPassword] = useState("");
+    let [repeatedPass,setRepeatedPass] = useState("");
+
+  
+    function handlesubmit(event){
+        event.preventDefault();
+        fetch ("http://localhost:5000/SignUp", {
+            method :"POST",
+            headers :{'Content-Type': 'application/json',
+                      'Accept': 'Application/json'},
+            body: JSON.stringify({
+                fullname,
+                email,
+                phoneNumber,
+                password,
+                repeatedPass
+            })
+        })
+    }
+
     return(
         <div className='article-signUp'>
             <article className="card-body mx-auto" style={{maxWidth: '400px'}}>
                 <h4 className="card-title mt-3 text-center">Create Account</h4>
                 <p className="text-center">Get started with your free account</p>
-                <form>
+                <form onSubmit={handlesubmit}>
                     <div className="form-group input-group">
                         <div className="input-group-prepend">
                             <span className="input-group-text"> <i className="fa fa-user"></i> </span>
                         </div>
-                        <input name="" className="form-control" placeholder="Full name" type="text" />
+                        <input name="" className="form-control" value={fullname} onChange={(event)=> setFullname(event.target.value)} placeholder="Full name" type="text" />
                     </div> 
                     {/* <!-- form-group// --> */}
                     <div className="form-group input-group">
                         <div className="input-group-prepend">
                             <span className="input-group-text"> <i className="fa fa-envelope"></i> </span>
                         </div>
-                        <input name="" className="form-control" placeholder="Email address" type="email" />
+                        <input name="" className="form-control" value={email} onChange={(event)=> setEmail(event.target.value)} placeholder="Email address" type="email" />
                     </div> 
                     {/* <!-- form-group// --> */}
                     <div className="form-group input-group">
@@ -27,21 +54,21 @@ let SignUp = ()=>{
                         <select className="custom-select" style={{maxWidth: '120px'}}>
                             <option defaultValue="">+212</option>
                         </select>
-                        <input name="" className="form-control" placeholder="Phone number" type="text" />
+                        <input name="" className="form-control" value={phoneNumber} onChange={(event)=> setPhoneNumber(event.target.value)} placeholder="Phone number" type="text" />
                     </div> 
                     {/* <!-- form-group end.// --> */}
                     <div className="form-group input-group">
                         <div className="input-group-prepend">
                             <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
                         </div>
-                        <input className="form-control" placeholder="Create password" type="password" />
+                        <input className="form-control" placeholder="Create password" value={password} onChange={(event)=> setPassword(event.target.value)} type="password" />
                     </div> 
                     {/* <!-- form-group// --> */}
                     <div className="form-group input-group">
                         <div className="input-group-prepend">
                             <span className="input-group-text"> <i className="fa fa-lock"></i> </span>
                         </div>
-                        <input className="form-control" placeholder="Repeat password" type="password" />
+                        <input className="form-control" value={repeatedPass} placeholder="Repeat password" type="password" onChange={(event)=> setRepeatedPass(event.target.value)}/>
                     </div> 
                     {/* <!-- form-group// -->*/}
                     <div className="form-group">
@@ -53,4 +80,6 @@ let SignUp = ()=>{
         </div>
     )
 }
+
+
 export default SignUp;
