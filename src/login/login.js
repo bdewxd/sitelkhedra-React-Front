222b87fdce52d091df1df5d2f225c91101179fb8
bdewxd <nimakdk@gmail.com>
2021-09-23 00:46:01 +0100

    e

--- src/login/login.js
+++ src/login/login.js
@@ -1,28 +1,44 @@
 import { useState } from "react";
+import { Redirect } from "react-router";
 
 let Login = ()=>{
     let [email, setEmail] = useState("");
     let [password, setPassword] = useState('');
-
+    let [errMsg, setErrMsg] = useState('');
+    let [url, setUrl] = useState('');
     function handlesubmit(event){
         event.preventDefault();
-
+        setErrMsg('');
         fetch('http://localhost:5000/login', {
             method: "POST",
-            headers: {
-                "Content-Type" : "application/json",
-                "accept": "application/json"
-            },
+            headers :{'Content-Type': 'Application/json',
+                      'Accept': 'Application/json',
+                      'Access-Control-Allow-Origin': 'https://localhost:5000'},
+            credentials: "include",
+            withCredentials: true,
             body: JSON.stringify({email, password})
         })
-        .then(e=> console.log(e))
+        .then(res=> {
+            if(res.status == 404){
+                res.json().then(({errMsg}) => setErrMsg(errMsg));
+            }else{
+                setUrl('profile');
+            }
+        })
+        
         .catch(err=> console.log(err))
+    }
+    if(url){
+        return <Redirect to={url} />
     }
     return (
         <div>
             <article className="card-body mx-auto" style={{maxWidth: '400px'}}>
                 <h4 className="card-title mt-3 text-center">Login to your account</h4>
                 <p className="text-center">get all the features</p>
+                {errMsg && <div className="alert alert-danger" role="alert">
+                    {errMsg}
+                </div>}
                 <form onSubmit={handlesubmit}>
                     <div className="form-group input-group">
                         <div className="input-group-prepend">
